9fba5ab9aecf92fab9771cd9f2b1046e1eb28901
haimana <haimanaare@gmail.com>
2024-10-04 11:57:30 +0300

    added logic for login

--- store/views.py
+++ store/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import *
 from django.contrib.auth import authenticate, login,logout
 from django.contrib import messages
@@ -13,7 +13,13 @@ def about(request):
     return render(request,'about.html')
 
 def login_user(request):
+    if request.method =='POST':
+        username = request.POST('username')
+        password = request.POST('password')
+        user = authenticate(request, username=username, password = password)
+        
     return render(request, 'login.html')
 
 def logout_user(request):
-    pass
+    logout(request)
+    return redirect('home')
