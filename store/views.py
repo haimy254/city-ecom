50a4d73c593f91c818843a3743d7fb9d40cba0f1
haimana <haimanaare@gmail.com>
2024-11-04 23:55:07 +0300

    miner changes in register and its working perfect

--- store/views.py
+++ store/views.py
@@ -18,12 +18,12 @@ def about(request):
 
 def login_user(request):
     if request.method =='POST':
-        username = request.POST('username')
-        password = request.POST('password')
+        username = request.POST['username']
+        password = request.POST['password']
         user = authenticate(request, username=username, password = password)
         if user is not None:
             login(request,user)
-            messages.success(request("You Have Been Loged In"))
+            # messages.success(request("You Have Been Loged In"))
             return redirect('home')
         else:
             messages.success(request("There Was An Error, Please Try Again"))
