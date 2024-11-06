0d940c51cc7d35f8115a316bbd52598dc1ee10e8
haimana <haimanaare@gmail.com>
2024-11-06 11:52:24 +0300

    created urls in cart

--- Cart/urls.py
+++ Cart/urls.py
@@ -2,5 +2,8 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-   
+   path('', views.cart_summery, name='cart_summery'),
+   path('add/', views.cart_add, name='cart_add'),
+   path('delete/', views.cart_delete, name='cart_delete'),
+   path('update', views.cart_update, name='cart_update'),
 ]
