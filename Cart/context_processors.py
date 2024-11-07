121642caf8f22d18cd2a1cea3253c3caafbebead
haimana <haimanaare@gmail.com>
2024-11-07 21:12:19 +0300

    connected with jquery

--- Cart/context_processors.py
+++ Cart/context_processors.py
@@ -1,15 +1,6 @@
+from .cart import Cart
 
-
-class Cart():
-    def __init__(self,request):
-        self.session = request.session
-
-        #get the currrent key of  the session
-        cart = self.session.get('session_key')
-
-        #if the user is new, no session key, we create one 
-        if 'session_key' not in request.session:
-            cart = self.session['session_key'] = {}
-
-        #make sure cart is available in all the pages of the site
-        self.cart = cart
+#create context processor so cart can work in all pages 
+def cart(request):
+    #return the default data form the cart
+    return {'cart':Cart(request)}
